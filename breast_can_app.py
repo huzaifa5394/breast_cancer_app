361be9975d4e080a842efa225c94f837743d0aeb
Huzaifa Asad <121677794+huzaifa5394@users.noreply.github.com>
2024-11-17 15:29:44 +0500

    Update breast_can_app.py

--- breast_can_app.py
+++ breast_can_app.py
@@ -1,14 +1,28 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import pickle
+#import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
-import os
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,accuracy_score
+from sklearn.model_selection import GridSearchCV
+#import os
 
-model_path = os.path.join(os.path.dirname(__file__), "logistic_regression_model.pkl")
-with open(model_path, "rb") as file:
-    model = pickle.load(file)
+#model_path = os.path.join(os.path.dirname(__file__), "logistic_regression_model.pkl")
+#with open(model_path, "rb") as file:
+    #model = pickle.load(file)
+Breast_Cancer_data=pd.read_csv("C:\Users\ROSHAN\Desktop\breast cancer project\my_app\brest_can_app\data.csv")
+Breast_Cancer_data["diagnosis"]=Breast_Cancer_data["diagnosis"].apply(lambda x: 1 if x=="M" else 0 )
+feature=Breast_Cancer_data.drop("diagnosis",axis=1).to_numpy()
+target=Breast_Cancer_data["diagnosis"].to_numpy()
+X_train, X_test, y_train, y_test = train_test_split(feature,target, test_size=0.2, random_state=42)
+clf=LogisticRegression()
+param={"penalty":["l1","l2","elasticnet"],"C":[1,2,3,4,5,6,8,10,20,30,40,50]}
+classifier=GridSearchCV(clf,param_grid=param,scoring='accuracy',cv=5)
+classifier.fit(X_train,y_train)
+y_pred=classifier.predict(X_test)
 # Data description
 st.title("Breast Cancer Prediction Dashboard")
 st.header("Data overview")
@@ -44,7 +58,7 @@ input_data = [
 
 def predict_dignose(input_data):
     data=np.array(input_data).reshape(1,-1)
-    prediction=model.predict(data)
+    prediction=classifier.predict(data)
     return "Malignant" if prediction[0]==1 else "Benign"
 
 if st.button("Predict"):
